83523ab7a9b6c9745cc663a6ce497a930848734c
Ashar4774 <ashar.muhammad74@gmail.com>
2025-02-07 21:56:00 +0500

    initially commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <TresCanvas clear-color="#82DBC5">
+    <TresPerspectiveCamera :position="[1, 3, 3]" :look-at="[0, 0, 0]" />
+    <TresMesh :position="[0, 0, 0]">
+      <TresBoxGeometry :args="[1, 1, 1]" />
+      <TresMeshBasicMaterial  color="teal" />
+    </TresMesh>
+    <TresAxesHelper/>
+  </TresCanvas>
+  <TresAmbientLight :intensity="1" />
+</template>
+
+<script setup>
+import { TresCanvas } from '@tresjs/core';
+
+</script>
+
+<style>
+
+html,
+body,
+#app{
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  width: 100%;
+}
+</style>
---
+++ src/components/Index.vue
@@ -0,0 +1,22 @@
+<template>
+  <TresCanvas >
+
+
+  </TresCanvas>
+</template>
+
+<script setup>
+  import { TresCanvas } from '@tresjs/core';
+
+</script>
+
+<style scoped>
+
+html,
+body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  width: 100%;
+}
+</style>
