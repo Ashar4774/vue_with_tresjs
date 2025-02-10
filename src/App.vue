b92b0e772372bad77c1ea6639b344c84425f4677
Ashar4774 <ashar.muhammad74@gmail.com>
2025-02-10 19:27:41 +0500

    animated material

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <TresCanvas clear-color="#82DBC5">
-    <TresPerspectiveCamera :position="[1, 3, 3]" :look-at="[0, 0, 0]" />
-    <TresMesh :position="[0, 0, 0]">
-      <TresBoxGeometry :args="[1, 1, 1]" />
-      <TresMeshBasicMaterial  color="teal" />
+    <Stats />
+    <TresPerspectiveCamera />
+    <TresMesh :position="[0, 0, 0]" ref="cubeRef">
+      <TresBoxGeometry />
+      <TresMeshBasicMaterial  color="yellow" />
     </TresMesh>
     <TresAxesHelper/>
   </TresCanvas>
@@ -11,7 +12,34 @@
 </template>
 
 <script setup>
-import { TresCanvas } from '@tresjs/core';
+import {ref, shallowRef, watchEffect} from 'vue';
+import { TresCanvas, useRenderLoop } from '@tresjs/core';
+import { Stats  } from '@tresjs/cientos';
+
+const { onLoop } = useRenderLoop();
+
+const cubeRef = shallowRef();
+
+onLoop(({delta, elapsed})=>{
+  if(cubeRef.value){
+    cubeRef.value.rotation.y += delta;
+    cubeRef.value.rotation.z = elapsed;
+
+    // cubeRef.value.position.x = Math.sin(elapsed * 2);
+    // cubeRef.value.position.y = Math.sin(elapsed * 2);
+    // cubeRef.value.position.z = Math.sin(elapsed * 2);
+
+    /*cubeRef.value.scale.set(
+        Math.sin(elapsed * 2),
+        Math.sin(elapsed * 2),
+        Math.sin(elapsed * 2)
+    )*/
+  }
+})
+
+watchEffect(()=>{
+  console.log(cubeRef);
+})
 
 </script>
 
